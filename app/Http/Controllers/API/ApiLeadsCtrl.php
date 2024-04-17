109f602ec26e5a50d0560cf9c7ee014d4be350f2
Breno de Carvalho <brenno_cmm@hotmail.com>
2024-04-17 14:30:18 -0300

    Ajustes telefone API

--- app/Http/Controllers/API/ApiLeadsCtrl.php
+++ app/Http/Controllers/API/ApiLeadsCtrl.php
@@ -54,7 +54,7 @@ class ApiLeadsCtrl extends Controller
             ];
             foreach($array as $ar){
                 if( $ar ){
-                    $tel = preg_replace( '/\D/', '', str_replace( array('+55'), array(''), intval($ar) ));
+                    $tel = preg_replace( '/\D/', '', str_replace( '+55', '', intval($ar) ));
                     $ddd = substr( $tel, 0, 2 );
                     $number = substr( $tel, 2 );
 	                $number = strlen( $number ) <= 8  ? substr( str_repeat( '9', 9 ) . $number, 0, 9 ) : $number;
