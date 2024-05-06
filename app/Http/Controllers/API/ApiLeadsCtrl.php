b1340ba09e28a80d240630a3427b8d6148cae5dd
Breno de Carvalho <brenno_cmm@hotmail.com>
2024-05-06 17:11:39 -0300

    Ajustes api

--- app/Http/Controllers/API/ApiLeadsCtrl.php
+++ app/Http/Controllers/API/ApiLeadsCtrl.php
@@ -33,7 +33,7 @@ class ApiLeadsCtrl extends Controller
 
         Log::build([
             'driver' => 'single',
-            'path' => storage_path('logs/data-' . date('d/m/y') . '.log'),
+            'path' => storage_path('logs/data-{date("d-m-Y")}.log'),
         ])->info('Dados do lead recebido: {request}', ['request' => json_encode($request)] );
 
         /**
