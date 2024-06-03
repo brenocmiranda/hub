8b27d3d12ca1901828e6dcb11e4d7fbfdac62c65
Breno de Carvalho <brenno_cmm@hotmail.com>
2024-06-03 13:52:50 -0300

    Ajustes de retorno Leads

--- app/Http/Controllers/LeadsCtrl.php
+++ app/Http/Controllers/LeadsCtrl.php
@@ -25,8 +25,7 @@ class LeadsCtrl extends Controller
     
     public function index()
     {   
-        $leads = Leads::select('created_at', 'name', 'email', 'companies_id', 'buildings_id', 'leads_origins_id', 'batches_id', 'id')->orderBy('created_at', 'desc')->get();
-        return view('leads.index')->with( compact('leads') );
+        return view('leads.index')->with('leads', Leads::select('created_at', 'name', 'email', 'companies_id', 'buildings_id', 'leads_origins_id', 'batches_id', 'id')->orderBy('created_at', 'desc')->get());
     }
 
     public function create()
