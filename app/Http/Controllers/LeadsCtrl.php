001fdff59e1c206d00de03dc24b6a2a403e58439
Breno de Carvalho <brenno_cmm@hotmail.com>
2024-06-03 13:40:45 -0300

    Ajustes de retorno Leads

--- app/Http/Controllers/LeadsCtrl.php
+++ app/Http/Controllers/LeadsCtrl.php
@@ -24,8 +24,9 @@ class LeadsCtrl extends Controller
 	}
     
     public function index()
-    {
-        return view('leads.index')->with('leads', Leads::select('created_at', 'name', 'email', 'companies_id', 'buildings_id', 'leads_origins_id', 'batches_id', 'id')->orderBy('created_at', 'desc')->get());
+    {   
+        $leads = Leads::select('created_at', 'name', 'email', 'companies_id', 'buildings_id', 'leads_origins_id', 'batches_id', 'id')->orderBy('created_at', 'desc')->get();
+        return view('leads.index')->with( compact('leads') );
     }
 
     public function create()
--- resources/views/base/nav.blade.php
+++ resources/views/base/nav.blade.php
@@ -24,7 +24,7 @@
                     </form>
                 </div>
             </div>
-            <div class="col-lg-6 col-5 d-flex justify-content-end gap-3">
+            <div class="col-lg-6 col-5 d-flex justify-content-end gap-3 pe-0">
                 <div class="settings d-flex align-items-center">
                     <div class="notify">
                         <a href="#" class="text-decoration-none text-dark">
