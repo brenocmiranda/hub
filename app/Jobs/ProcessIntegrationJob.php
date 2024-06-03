108c9e73b95f4daa2c9d4c911419e5aa94a53231
Breno de Carvalho <brenno_cmm@hotmail.com>
2024-06-03 15:03:22 -0300

    Revert commit

--- app/Jobs/ProcessIntegrationJob.php
+++ app/Jobs/ProcessIntegrationJob.php
@@ -48,7 +48,7 @@ class ProcessIntegrationJob implements ShouldQueue
                     '$email' => $this->lead->email,
                     '$origin' => $this->lead->RelationOrigins->name,
                     '$nomeEmpreendimento' => $this->lead->RelationBuildings->name,
-                    '$pp' => $this->lead->RelationFields->where('name', 'pp')->last() ? $this->lead->RelationFields->where('name', 'pp')->last()->value : 'N',
+                    '$pp' => $this->lead->RelationFields->where('name', 'pp')->last() ? $this->lead->RelationFields->where('name', 'pp')->last()->value : 'Y',
                     '$utm_source' => $this->lead->RelationFields->where('name', 'utm_source')->last() ? $this->lead->RelationFields->where('name', 'utm_source')->last()->value : '',
                     '$utm_xrm' => $this->lead->RelationFields->where('name', 'utm_source')->last() ? self::get_utm_source_by_valor( $this->lead->RelationFields->where('name', 'utm_source')->last()->value ) : self::get_utm_source_by_valor( "default" ),
                     '$utm_medium' => $this->lead->RelationFields->where('name', 'utm_medium')->last() ? $this->lead->RelationFields->where('name', 'utm_medium')->last()->value : '',
