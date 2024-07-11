7a903d9f98d878c41475359883e7ffee4cc3a909
Breno de Carvalho <brenno_cmm@hotmail.com>
2024-07-10 23:01:32 -0300

    Adiamento de GTM e Minify js

--- public/js/external/form.js
+++ public/js/external/form.js
@@ -266,6 +266,38 @@ jQuery( function( $ ){
 	});
 
 	/**
+	 * Adiar o carregamento do GTM
+	*/
+	if (window.ID_GTM) {
+		document.addEventListener('DOMContentLoaded', () => {
+			/** init gtm after 3500 seconds - this could be adjusted */
+			setTimeout(initGTM, 3500);
+		});
+		document.addEventListener('scroll', initGTMOnEvent);
+		document.addEventListener('mousemove', initGTMOnEvent);
+		document.addEventListener('touchstart', initGTMOnEvent);
+	}
+	function initGTMOnEvent(event) {
+		initGTM();
+		event.currentTarget.removeEventListener(event.type, initGTMOnEvent); // remove the event listener that got triggered
+	}
+	function initGTM() {
+		if (window.gtmDidInit) {
+			return false;
+		}
+		window.gtmDidInit = true; // flag to ensure script does not get added to DOM more than once.
+		const script = document.createElement('script');
+		script.type = 'text/javascript';
+		script.async = true;
+		// ensure PageViews is always tracked (on script load)
+		script.onload = () => {
+			dataLayer.push({ event: 'gtm.js', 'gtm.start': new Date().getTime(), 'gtm.uniqueEventId': 0 });
+		};
+		script.src = 'https://www.googletagmanager.com/gtm.js?id=' + window.ID_GTM;
+		document.head.appendChild(script);
+	}
+
+	/**
 	 * Leitura de section por viewport (Adicionando class is-view quando estiver disponível a section)
 	*/
 	$.fn.isInViewport = function () {
