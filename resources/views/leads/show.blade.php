f17bdd928c68f8eb5ad8f3ce1d900f0efa7ca6fb
Breno de Carvalho <brenno_cmm@hotmail.com>
2024-08-19 15:11:36 -0300

    Ajuste imóvelweb

--- resources/views/leads/show.blade.php
+++ resources/views/leads/show.blade.php
@@ -122,7 +122,7 @@ Detalhes do lead
                                         <h6 class="fw-bold w-75">T: {{ $log->attempts }} - Execução do processo de <span class="text-decoration-underline">{{$log->RelationIntegrations->name}}</span>.</h6>
                                         <span href="#" class="me-auto ms-auto-md w-25 text-left text-md-end mb-3 mb-md-0">{{ $log->created_at->format("d/m/Y H:i:s") }}</span>
                                         <p class="w-100">A tentativa de envio do lead para integração resultou em <strong class="text-success">sucesso</strong>.</p>
-                                        <small class="text-break d-block w-100 ps-3">{{ json_decode($log->RelationPipelinesLog->response) }}.</small>
+                                        <small class="text-break d-block w-100 ps-3">{{ json_validate($log->RelationPipelinesLog->response) ? json_decode($log->RelationPipelinesLog->response) : $log->RelationPipelinesLog->response }}.</small>
                                     </li>
                                 @elseif($log->statusCode >= 400 || $log->statusCode <= 500)
                                     <li class="d-flex flex-wrap flex-column flex-md-row">
