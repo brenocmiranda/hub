4a973e63d97a403b76e2e054583b5a06c94f68a5
Breno de Carvalho <brenno_cmm@hotmail.com>
2024-09-23 16:07:39 -0300

    Leads ajuste in reports

--- app/Exports/LeadsExport.php
+++ app/Exports/LeadsExport.php
@@ -33,13 +33,12 @@ class LeadsExport implements FromView, WithEvents
     public function view(): View
     {   
         $leads = Leads::whereDate('created_at', '>=', $this->dataInicial)->whereDate('created_at', '<=', $this->dataFinal);
-
-        if( Gate::check('access_komuh') != 1 ) {
-            $leads->where('companies_id', $this->company);
-        }
-
         $leads = $this->building ? $leads->where('buildings_id', $this->building) : $leads;
         $leads = $this->origem ? $leads->where('leads_origins_id', $this->origem) : $leads;
+
+        if( Gate::check('access_komuh') ) {
+            $leads->where('companies_id', $this->company);
+        }
 
         return view('vendor.exports.leads', [
             'leads' => $leads->get(),
