bf67bbf9bf27a3a067f096e90fb014b2cc1166f8
Breno de Carvalho <brenno_cmm@hotmail.com>
2024-10-17 14:47:45 -0300

    whereLike API Leads

--- app/Http/Controllers/API/ApiLeadsCtrl.php
+++ app/Http/Controllers/API/ApiLeadsCtrl.php
@@ -139,7 +139,7 @@ class ApiLeadsCtrl extends Controller
             $bdefault = BuildingsKeys::join('buildings_partners', 'buildings_partners.buildings_id', 'buildings_keys.buildings_id')
                                             ->where('buildings_partners.companies_id', $companies_id)
                                             ->where('buildings_partners.main', 1)
-                                            ->whereLike('buildings_keys.value', '%default%')
+                                            ->whereLike('buildings_keys.value', '%defaultepo%')
                                             ->where('buildings_keys.active', 1)
                                             ->first();
                                             var_dump($bdefault);
